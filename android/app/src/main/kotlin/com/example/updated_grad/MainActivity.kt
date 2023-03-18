148545e968fa0053eb820d0648fca55f0972f911
ZaidJabri <zaidjabri1@hotmail.com>
2023-03-18 12:20:28 +0300

    First

---
+++ android/app/src/main/kotlin/com/example/updated_grad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.updated_grad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:maps_toolkit/maps_toolkit.dart' as maps_toolkit;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:updated_grad/local_notifications.dart';
+import 'notification_service.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print("This is message form background");
+  print(message.notification?.title);
+  print(message.notification?.body);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  runApp(GeofenceMap());
+}
+
+class GeofenceMap extends StatefulWidget {
+  @override
+  _GeofenceMapState createState() => _GeofenceMapState();
+}
+
+class _GeofenceMapState extends State<GeofenceMap> {
+  late String notificationMSG;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  final LatLng _center = const LatLng(31.872378, 35.885011);
+  Set<Circle> _circles = {};
+  Set<Polygon> _polygons = {};
+  late GeolocatorPlatform _geolocator;
+  bool _isInsideGeofence = false;
+
+  @override
+  void initState() {
+    LocalNotificationService.initilize();
+    super.initState();
+    _geolocator = GeolocatorPlatform.instance;
+    _initGeofencing();
+
+    NotificationService.initialize(flutterLocalNotificationsPlugin);
+    //////////
+    // terminated state
+    FirebaseMessaging.instance.getInitialMessage().then((event) {});
+
+    // foreground state
+    FirebaseMessaging.onMessage.listen((event) {
+      LocalNotificationService.showNotificationOnForeground(event);
+    });
+
+    // background state
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {});
+  }
+
+  void _initGeofencing() async {
+    // Check if we have permission to access the user's location
+    LocationPermission permission = await _geolocator.checkPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      print('Permission denied to access location');
+      return;
+    }
+
+    // Subscribe to location updates
+    _geolocator.getPositionStream().listen((position) {
+      bool isInsideGeofence = _isPositionInsideGeofence(position);
+      if (isInsideGeofence != _isInsideGeofence) {
+        setState(() {
+          _isInsideGeofence = isInsideGeofence;
+        });
+        if (_isInsideGeofence) {
+          _onEnterGeofence();
+        } else {
+          _onExitGeofence();
+        }
+      }
+    });
+  }
+
+  bool _isPositionInsideGeofence(Position position) {
+    double distance = Geolocator.distanceBetween(
+      position.latitude,
+      position.longitude,
+      _center.latitude,
+      _center.longitude,
+    );
+
+    // Check if the position is inside the circle
+    if (distance <= 100) {
+      return true;
+    }
+
+    // Check if the position is inside the polygon
+    List<maps_toolkit.LatLng> polygonLatLngs = _polygons.first.points
+        .map((point) => maps_toolkit.LatLng(point.latitude, point.longitude))
+        .toList();
+    maps_toolkit.LatLng positionLatLng =
+        maps_toolkit.LatLng(position.latitude, position.longitude);
+    bool isInsidePolygon = maps_toolkit.PolygonUtil.isLocationOnEdge(
+        positionLatLng, polygonLatLngs, tolerance: 100, true);
+
+    return isInsidePolygon;
+  }
+
+  void _onEnterGeofence() {
+    print('Entered geofence');
+    FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+    // TODO: Handle enter geofence event
+    NotificationService.showBigTextNotification(
+        title: "Geofence",
+        body: "Danger!!",
+        fln: flutterLocalNotificationsPlugin);
+  }
+
+  void _onExitGeofence() {
+    print('Exited geofence');
+    // TODO: Handle exit geofence event
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _circles.add(Circle(
+      circleId: CircleId('Geofence'),
+      center: _center,
+      radius: 200,
+      fillColor: Colors.blue.withOpacity(0.5),
+      strokeColor: Colors.blue,
+    ));
+    _polygons.add(Polygon(
+      polygonId: PolygonId('Geofence'),
+      points: [
+        LatLng(31.9397339197085, 35.8867618197085),
+        LatLng(31.9424318802915, 35.8894597802915),
+      ],
+      fillColor: Colors.blue.withOpacity(0.5),
+      strokeColor: Colors.blue,
+    ));
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Geofence Map'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 16.0,
+        ),
+        circles: _circles,
+        polygons: _polygons,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+      ),
+    ));
+  }
+}
