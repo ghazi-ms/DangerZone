2841ac5d139cdd192faf4e175fb5f646941ba36d
ghazi alsalah <ghazi.ms@hotmail.com>
2023-04-15 16:48:12 +0300

    Update MainLayout.dart

--- lib/serivces/MainLayout.dart
+++ lib/serivces/MainLayout.dart
@@ -1,19 +1,26 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart';
 import 'package:updated_grad/widgets/cards.dart';
+
 class MainLayout extends StatefulWidget {
   TextEditingController coor = TextEditingController();
-  LatLng center ;
+  LatLng center;
+
   Set<Circle> circles = {};
   Set<Polygon> polygons = {};
   late List<String> lists;
   late List<String> tests;
 
-  MainLayout(this.coor, this.center, this.circles, this.polygons,this.lists,this.tests);
+  MainLayout(this.coor, this.center, this.circles, this.polygons, this.lists,
+      this.tests);
 
   @override
-  State<MainLayout> createState() => _MainLayoutState(this.lists,this.tests);
+  State<MainLayout> createState() => _MainLayoutState(this.lists, this.tests);
 }
 
 class _MainLayoutState extends State<MainLayout> {
@@ -21,13 +28,11 @@ class _MainLayoutState extends State<MainLayout> {
   List<String> test;
 
   late String dropdownValue;
-  _MainLayoutState(this.list,this.test);
+
+  _MainLayoutState(this.list, this.test);
 
   @override
-
-
   void changeDrop() {
-
     setState(() {
       final data = dropdownValue.toString().split(",");
       widget.center = LatLng(double.parse(data[0]), double.parse(data[1]));
@@ -38,17 +43,15 @@ class _MainLayoutState extends State<MainLayout> {
         fillColor: Colors.red.withOpacity(0.5),
         strokeColor: Colors.red,
       );
-      if(widget.circles.contains(c)){
+      if (widget.circles.contains(c)) {
         print("contains");
-      }else{
+      } else {
         widget.circles.add(c);
       }
-
     });
-
-
   }
-  Polygon p=Polygon(
+
+  Polygon p = Polygon(
     polygonId: PolygonId(const Time().second.toString()),
     points: [
       LatLng(31.8288, 35.9010),
@@ -59,15 +62,13 @@ class _MainLayoutState extends State<MainLayout> {
     fillColor: Colors.blue.withOpacity(0.5),
     strokeColor: Colors.blue,
   );
+
   void add() {
-
-
     setState(() {
       widget.polygons.add(p);
 
-
-    if (widget.coor.text !="") {
-      list.add(widget.coor.text);
+      if (widget.coor.text != "") {
+        list.add(widget.coor.text);
 
         final data = widget.coor.toString().split(",");
 
@@ -79,92 +80,102 @@ class _MainLayoutState extends State<MainLayout> {
           fillColor: Colors.red.withOpacity(0.5),
           strokeColor: Colors.red,
         );
-        if(widget.circles.contains(c)){
-
-        }else{
+        if (widget.circles.contains(c)) {
+        } else {
           widget.circles.add(c);
         }
-
       }
     });
-
   }
+  List<dynamic> thelist =[];
+  int tog=0;
 
+  Future getData() async {
+    print("start");
+    const apiKey = 'http://192.168.0.107:5000/';
+    try {
+      Response response = await get(Uri.parse('$apiKey'));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          thelist=json.decode(response.body);
+          tog=1;
+        });
+        List<dynamic> data = thelist;
+
+        for (var item in data) {
+          // List<String> co = [''];
+          // co=item['coordinates'];
+          // for (var i in co) {
+          //   print(i);
+          // }
+
+          print(item['title'] +
+              "-----" +
+              item['Locations'] +
+              "-------" +
+              item['id']);
+        }
+      } else {
+        print(response.body);
+        throw 'Problem with the get request';
+      }
+    } catch (e) {
+      print(e.toString() + " error");
+    }
+
+    print("done");
+  }
 
   Widget build(BuildContext context) {
 
     dropdownValue = list[0];
     return SingleChildScrollView(
-      child: Column(
-        children:[
-                (widget.circles.isEmpty && widget.polygons.isEmpty) ? const Text("Empty") :
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height*0.40,
-                      child: GoogleMap(
-                        initialCameraPosition: CameraPosition(
-                          target: widget.center,
-                          zoom: 16.0,
-                        ),
-                        circles: widget.circles,
-                        polygons: widget.polygons,
-                        myLocationEnabled: true,
-                        myLocationButtonEnabled: true,
-                      ),
-                    ),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height*0.50,
-                          child: Column(
-                           children: [ Container(
-                              child: Column(
-                                children: [
-                                DropdownButton<String>(
-                                  value: dropdownValue,
-                                  icon: const Icon(Icons.arrow_downward),
-                                  elevation: 16,
-                                  style: const TextStyle(color: Colors.deepPurple),
-                                  underline: Container(
-                                    height: 2,
-                                    color: Colors.deepPurpleAccent,
-                                  ),
-                                  items: list.map<DropdownMenuItem<String>>((String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList(),
-                                  onChanged: (String? value) {
+      child: Column(children: [
 
-                                    // This is called when the user selects an item.
-                                    setState(() {
-                                      dropdownValue = value!;
-                                      changeDrop();
-                                    });
-                                  },
-                                ),
-                                TextField(
-                                  controller: widget.coor,
-                                ),
-                                ElevatedButton(onPressed: add, child: const Text("Add")), ]
-                        ),
-    ),
+        ElevatedButton(onPressed: getData, child: Text("get Data")),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.50,
+          child: Column(children: [
+            Container(
+              child: Column(children: [
+                DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      dropdownValue = value!;
+                      changeDrop();
+                    });
+                  },
+                ),
+                TextField(
+                  controller: widget.coor,
+                ),
+                ElevatedButton(onPressed: add, child: const Text("Add")),
+              ]),
+            ),
 
-                                Cards(test),
+            tog==1?Cards(thelist):Text(tog.toString()),
 
-
-
-
-                            //////////
-                          ]
-                          ),
-
-
-
-                          ),
-                          ]
-                        ),
-
-          );
+            //////////
+          ]),
+        ),
+      ]),
+    );
   }
 }
